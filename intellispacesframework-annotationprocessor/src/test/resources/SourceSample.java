92e6f054b374965a7e9fee030015f6df354a982f
lavor <lavor.andrey@gmail.com>
2024-06-28 12:08:15 +0600

    JavaArtifactContext corrected

--- intellispacesframework-annotationprocessor/src/test/resources/SourceSample.java
+++ intellispacesframework-annotationprocessor/src/test/resources/SourceSample.java
@@ -1,7 +1,5 @@
 package tech.intellispaces.framework.annotationprocessor;
 
-import tech.intellispaces.framework.annotationprocessor.AnnotationSample;
-
 @AnnotationSample(enableAutoGenerate = true)
 public interface SourceSample {
 }
