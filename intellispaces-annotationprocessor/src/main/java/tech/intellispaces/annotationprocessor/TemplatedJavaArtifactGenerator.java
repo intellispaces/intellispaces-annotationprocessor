c9652f56a98bb03be5618fc942ff11c72703052e
lavor <lavor.andrey@gmail.com>
2024-12-23 10:38:08 +0600

    Update TemplatedJavaArtifactGenerator. Use MutableImportList

--- intellispaces-annotationprocessor/src/main/java/tech/intellispaces/annotationprocessor/TemplatedJavaArtifactGenerator.java
+++ intellispaces-annotationprocessor/src/main/java/tech/intellispaces/annotationprocessor/TemplatedJavaArtifactGenerator.java
@@ -1,11 +1,10 @@
 package tech.intellispaces.annotationprocessor;
 
-import tech.intellispaces.general.collection.ArraysFunctions;
-import tech.intellispaces.general.exception.UnexpectedExceptions;
 import tech.intellispaces.general.type.ClassFunctions;
 import tech.intellispaces.general.type.ClassNameFunctions;
-import tech.intellispaces.general.type.PrimitiveFunctions;
 import tech.intellispaces.java.reflection.customtype.CustomType;
+import tech.intellispaces.java.reflection.customtype.ImportLists;
+import tech.intellispaces.java.reflection.customtype.MutableImportList;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -20,7 +19,7 @@ import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
 public abstract class TemplatedJavaArtifactGenerator extends TemplatedArtifactGenerator {
   private final Set<String> staticImports = new HashSet<>();
-  private final HashMap<String, String> imports = new HashMap<>();
+  private final MutableImportList imports = ImportLists.get(this::generatedArtifactName);
   private final List<String> javaDocLines = new ArrayList<>();
   private final Map<String, Object> templateVariables = new HashMap<>();
 
@@ -30,23 +29,27 @@ public abstract class TemplatedJavaArtifactGenerator extends TemplatedArtifactGe
   }
 
   public void addImport(Class<?> aClass) {
-    addImport(aClass.getName());
-  }
-
-  public void addImports(Class<?>... classes) {
-    ArraysFunctions.foreach(classes, this::addImport);
+    imports.add(aClass);
   }
 
   public void addImport(String canonicalName) {
-    if (ClassFunctions.isLanguageClass(canonicalName)) {
-      return;
-    }
-    String simpleName = ClassNameFunctions.getSimpleName(canonicalName);
-    imports.putIfAbsent(simpleName, canonicalName);
+    imports.add(canonicalName);
+  }
+
+  public void addImports(Class<?>... classes) {
+    imports.add(classes);
   }
 
   public Consumer<String> getImportConsumer() {
     return this::addImport;
+  }
+
+  public String addImportAndGetSimpleName(Class<?> aClass) {
+    return imports.addAndGetSimpleName(aClass);
+  }
+
+  public String addImportAndGetSimpleName(String canonicalName) {
+    return imports.addAndGetSimpleName(canonicalName);
   }
 
   public void addStaticImport(String canonicalName) {
@@ -58,37 +61,11 @@ public abstract class TemplatedJavaArtifactGenerator extends TemplatedArtifactGe
   }
 
   public String simpleNameOf(Class<?> aClass) {
-    return simpleNameOf(aClass.getCanonicalName());
+    return imports.simpleNameOf(aClass);
   }
 
   public String simpleNameOf(String canonicalName) {
-    String simpleName = ClassNameFunctions.getSimpleName(canonicalName);
-    if (ClassFunctions.isLanguageClass(canonicalName)) {
-      return simpleName;
-    }
-    if (simpleName.equals(generatedArtifactSimpleName())) {
-      return canonicalName;
-    }
-
-    String importedCanonicalName = imports.get(simpleName);
-    if (importedCanonicalName == null) {
-      throw UnexpectedExceptions.withMessage("Class {0} is missing from list of imported classes",
-          canonicalName);
-    }
-    if (canonicalName.equals(importedCanonicalName)) {
-      return simpleName;
-    }
-    return canonicalName;
-  }
-
-  public String addToImportAndGetSimpleName(String canonicalName) {
-    addImport(canonicalName);
-    return simpleNameOf(canonicalName);
-  }
-
-  public String addToImportAndGetSimpleName(Class<?> aClass) {
-    addImport(aClass);
-    return simpleNameOf(aClass);
+    return imports.simpleNameOf(canonicalName);
   }
 
   public void addVariable(String name, Object value) {
@@ -135,13 +112,7 @@ public abstract class TemplatedJavaArtifactGenerator extends TemplatedArtifactGe
   }
 
   private List<String> getImports() {
-    return imports.values().stream()
-        .filter(className -> !ClassNameFunctions.getSimpleName(className).equals(generatedArtifactSimpleName()))
-        .filter(className -> !ClassNameFunctions.getPackageName(className).equals(generatedArtifactPackageName()))
-        .filter(className -> !ClassFunctions.isLanguageClass(className))
-        .filter(className -> !PrimitiveFunctions.isPrimitiveTypename(className))
-        .sorted()
-        .toList();
+    return imports.getImports();
   }
 
   private List<String> getStaticImports() {
